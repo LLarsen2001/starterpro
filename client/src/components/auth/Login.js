02e7af11346a65c124a39fea7ce7fc838271ed52
Lance Larsen <lancelarsen2001@gmail.com>
2022-06-21 09:49:36 -0600

    login added

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,10 +1,42 @@
-import React from "react"
+import { useContext, useState } from "react"
+import { AuthContext } from "../../providers/AuthProvider"
+
 
 const Login = () => {
+    const { login } = useContext(AuthContext)
+    const [email, setEmail] = useState("test@test.com")
+    const [password, setPassword] = useState("123456")
+
+    const handleSubmit = (r) => {
+        r.preventDefault()
+
+        login({ email, password })
+    }
+
     return (
-        <div>
-            <h2>Login</h2>
-        </div>
+        <>
+            <h1>Login</h1>
+            <p>please enter your email and password to login.</p>
+            <form onSubmit={handleSubmit}>
+                <label>Email</label>
+                <input
+                    required
+                    autoFocus
+                    value={email}
+                    onChange={(r) => setEmail(r.target.value)}
+                />
+                <label>password</label>
+                <input
+                    minLength={6}
+                    required
+                    autoFocus
+                    value={password}
+                    onChange={(r) => setPassword(r.target.value)}
+                />
+
+                <button type="submit">Sign up.</button>
+            </form>
+        </>
     )
 }
 export default Login
